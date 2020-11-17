20308040ed558c5c2fb97bb3a9146676d0b19ee6
Asta Bevainyte <asta.bevainyte@sky.uk>
2020-11-17 23:14:29 +0000

    AB: mvvm

--- app/src/main/java/com/example/firestore/PageViewModel.kt
+++ app/src/main/java/com/example/firestore/PageViewModel.kt
@@ -1,21 +1,22 @@
 package com.example.firestore
-import android.widget.TextView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.Observer
+import androidx.lifecycle.*
 
+ //https://en.proft.me/2019/10/20/firebase-cloud-firestore-and-livedata/
+
+//https://stackoverflow.com/questions/54466799/retrieving-data-from-firestore-using-mvvm-architecture
+
+//https://www.ericdecanini.com/2020/01/27/android-chat-messenger-with-firestore-mvvm-edition-with-dagger-livedata-and-coroutines/
 
 class PageViewModel: ViewModel() {
-
-    private val cfm by lazy { FBRepository() }
-
-
+    private val postsValues = MutableLiveData<List<Page>>()
+    private val repo: FBRepository = FBRepository()
 
 
-    fun postsUpdate() {
+    fun onPostsValuesChange(): LiveData<List<Page>> {
+        repo.onPostsValuesChange()
+        return postsValues
     }
 
-
+    fun stopListeningForPostChanges() = repo.stopListeningForPostChanges()
 
 }
